bc642b9feb7b6413207eedbed0e3203f2732cd60
Oğuzhan Çevik <oguzhan728@gmail.com>
2022-03-20 23:36:36 +0300

    server edited

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express=require('express');
 const app=express();
-const cors=require('cors')
+const cors=require('cors'),
+mongoose=require("mongoose");
 
 app.use(cors())
 app.use(express.json())
+mongoose.connect("mongodb://localhost/Local_Guide", { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.post('/api/register',(req,res)=>{
     console.log(req.body)
