b0309db2142a03527559086febf24a402d6861d3
burakS-cloud <bsurumcuoglu@gmail.com>
2022-03-25 01:48:58 +0300

    search page responsiveness halfway done

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -19,18 +19,18 @@ const Home = () => {
   
   
 
-  console.log(watch().country)
+  // console.log(watch().country)
 
   let countryInput = watch().country ? watch().country : '';
   let stateInput = watch().state ? watch().state : '';
+  let cityInput = watch().city ? watch().city : '';
   let arrivalDate = watch().arriving ? watch().arriving : '';
   let minAge = watch().minAge ? watch().minAge : '';
   let maxAge = watch().maxAge ? watch().maxAge : '';
-  console.log(watch().state)
-  console.log(watch().arriving)
-  console.log(watch().minAge)
-  console.log(watch().maxAge)
-  
+  // console.log(watch().state)
+  // console.log(watch().arriving)
+  // console.log(watch().minAge)
+  // console.log(watch().maxAge)
   
   let states = [];
   let isFoundCountry = false;
@@ -41,22 +41,29 @@ const Home = () => {
       isFoundCountry = true;
       console.log("Finally a valid country input by user")
       states = State.getStatesOfCountry(country.isoCode);
-      console.log(states);
+
+      //  console.log(states);
     }  
   })
 
   let stateNameSelected = "";
   let chosenState = {};
+  let chosenStateArr = [];
+  
+  
 
-   states.forEach((city) => {
-      if(city.name === stateInput){
+   states.forEach((state) => {
+      if(state.name === stateInput){
         isFoundState = true;
-        stateNameSelected = city.name;
-        chosenState = city;
-        console.log("A valid state is selected")
+        stateNameSelected = state.name;
+        chosenState = state;
+        chosenStateArr = Object.values(chosenState);
+        // console.log("A valid state is selected")
+        // console.log("Chosen state as object",chosenState)
+        // console.log("Chosen state as array", chosenStateArr)
         // console.log("stateSelected variable", stateNameSelected, stateNameSelected);
         // console.log(typeof chosenState)
-        console.log(City.getCitiesOfState(chosenState.countryCode, chosenState.isoCode))
+        // console.log(City.getCitiesOfState(chosenState.countryCode, chosenState.isoCode))
         // console.log(City.getAllCities());
 
       }
@@ -78,84 +85,105 @@ const Home = () => {
 
     let dateStringToPass = `${year}-${finalMonth}-${day} `;
     let maxAgeArr = [];
+    let minAgeArr = [];
+    if(maxAge){
+      for(let i = 0; i < ageArr.length; i++){
+        if(maxAge >= ageArr[i]){
+          minAgeArr.push(ageArr[i])
+        }
+      }
+    } else {
+      minAgeArr = ageArr;
+    }
+
     for(let i = 0; i < ageArr.length; i++){
       if(minAge <= ageArr[i]){
         maxAgeArr.push(ageArr[i]);
       }
     }
-    // let finalDateToPass = new Date(dateStringToPass);
+
+    let selectedStatesIsoCode = chosenStateArr[1];
+    let selectedStatesCountryCode = chosenStateArr[2];
+    let filteredStates = states.filter(state => state.name.startsWith(stateInput));
+    let filteredCountries = Country.getAllCountries().filter(country => country.name.startsWith(countryInput));
+    let filteredCities = City.getCitiesOfState(selectedStatesCountryCode, selectedStatesIsoCode).filter(city => city.name.startsWith(cityInput));
+    
     
   
 
   return (
+    
+      
     <div className='img-bg'>
-      <div style={{display:'flex', justifyContent:'center'}}><h2 style={{paddingTop:'5%'}}>Search for Ads</h2></div>
+      <div id='heading'><h2>Search for Ads</h2></div>
       <div>
-      <form onSubmit={handleSubmit((data) => {
+      <form id="form" onSubmit={handleSubmit((data) => {
         console.log(data);
-      })} style={{display:'flex', justifyContent:'center', marginTop:'3%'}}>
+      })} >
 
-        <div className='form-group'>
+        <div className='row justify-content-md-center'>
+        <div className='form-group col col-lg-2'>
         <label htmlFor="country">Type in Country:</label>
-        <input  className='form-control' {...register("country")} name="country"  id='country' type="text" />
+        <input autoComplete='off' className='form-control' {...register("country")} list="countries" name="country"  id='country' type="text" />
+        <datalist id="countries">
+          {filteredCountries.map((country,key) => (
+            <option key={key} value={country.name}>{country.name}</option>
+          ))}
+        </datalist>
         </div>
 
-        <div className='form-group'>
+        <div className='form-group col col-lg-2'>
         <label htmlFor='state'>Choose State:</label>
-        <select disabled={!isFoundCountry} {...register("state", { required: true })} className='form-control'  name="state" id="state">
-          {/* The options will be created by mapping over the data that
-          comes back from the state api, for each city. */}
+        <input autoComplete='off' disabled={!isFoundCountry} className='form-control' {...register("state", { required: true })} type="text" name='state' id='state' list='states' />
+        <datalist name="states" id="states">
           <option selected disabled value="">Choose a State</option>
-          {/* <option value="Berlin">Berlin</option>
-          <option value="Rome">Rome</option>
-          <option value="Prague">Prague</option> */}
-          {states.map((state) => (
-            <option value={state.name}>{state.name}</option>
+          {filteredStates.map((state, key) => (
+            <option key={key} value={state.name}>{state.name}</option>
           ))}
           {/* <option value={city}>{city}</option> */}
-        </select>
+        </datalist>
         </div>
 
-        <div className='form-group'>
+        <div className='form-group col col-lg-2'>
         <label htmlFor='city'>Choose City:</label>
-        <select disabled={!isFoundState} {...register("city", { required: true })} className='form-control'  name="city" id="city">
-          {/* The options will be created by mapping over the data that
-          comes back from the state api, for each city. */}
+        <input autoComplete='off' className='form-control' disabled={!isFoundState} {...register("city", { required: true })} type="text" name='city' id='city' list='cities' />
+        <datalist name="cities" id="cities">
           <option selected disabled value="">Choose a City</option>
-          {/* <option value="Berlin">Berlin</option>
-          <option value="Rome">Rome</option>
-          <option value="Prague">Prague</option> */}
-          { City.getCitiesOfState(chosenState.countryCode,chosenState.isoCode).map((city,key)=> (
+          { filteredCities.map((city,key)=> (
             <option key={key} value={city.name}>{city.name}</option>
           ))}
-          {/* <option value={city}>{city}</option> */}
-        </select>
+        </datalist>
+        </div>
         </div>
 
-        {/* {`${new Date(dateStringToPass)}`} */}
+        <div className="w-100"></div>
         
-        <div className='form-group'>
+        <div className='row justify-content-md-center'>
+        <div className='form-group col col-lg-2'>
         <label htmlFor="arriving">Arriving:</label>
         <input min={`${year}-${finalMonth}-${day}`} max={`${new Date().getFullYear()+1}-${finalMonth}-${day}`} className='form-control' {...register("arriving", { required: 'You have to select an arrival date' })} name="arriving" id="arriving" type="date" />
         </div>
         
-        <div className='form-group'>
+        <div className='form-group col col-lg-2'>
         <label htmlFor='leaving'>Leaving:</label>
         <input min={`${arrivalDate}`} className='form-control' {...register("leaving", { required: true })} name='leaving' type="date" />
         </div>
+        </div>
+
+        <div className="w-100"></div>
         
-        <div style={{display:'flex'}} className='form-group'>
-        <div>
+        <div className='row justify-content-md-center'>
+        <div className='form-group col col-lg-1'>
         <label htmlFor='minAge'>From:</label>
         <select {...register("minAge", { required: 'You have to select minimum age' , min:{value: 18, message:'You have to select minimum age'} })} className='form-control'  name="minAge" id="minAge">
           <option selected disabled value="">Age</option>
-          {ageArr.map((item,key) => {
+          {minAgeArr.map((item,key) => {
             return <option key={key} value={item}>{item}</option>
           })}
         </select>
         </div>
 
-        <div>
+        <div className='form-group col col-lg-1'>
         <label htmlFor='maxAge'>To:</label>
         <select {...register("maxAge", { required: true })} className='form-control' name="maxAge" id="maxAge">
         <option selected disabled value="">Age</option>
@@ -165,22 +193,22 @@ const Home = () => {
            
         </select>
         </div>
-        </div>
         
-         <div className='form-group'>
+        
+        <div className='form-group col col-lg-1'>
         <label htmlFor='gender'>Gender:</label>
         <select {...register("gender", { required: true })} className='form-control' name="gender" id="gender">
           <option value="Male">Male</option>
           <option value="Female">Female</option> 
         </select>
         </div>
-        
-        {/* {...required("gender", { required: true })} */}
-         <button style={{height:'50%', marginTop:'2em'}} type="submit" className="btn btn-primary">Submit</button>
-         
+        <button style={{height:'50%', marginTop:'2em'}} type="submit" className="btn btn-primary">Submit</button>
+        </div>
+          
       </form>
       </div>
     </div>
+
     
   )
 }
